a6b8402ebcafa0a5ec1fcdbb8042b0b6e311601c
Takeshi347 <mateus2908pereira@gmail.com>
2025-04-10 17:46:06 +0000

    aaaa

---
+++ src/components/body.jsx
@@ -0,0 +1,7 @@
+export default function body() {
+    return (
+        <div>
+            <h1>Corpo da página</h1>
+        </div>
+    )
+}
---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer(){
+    return (
+        <div>
+            <div className="container-fluid fixed-bottom">
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-center">
+                        <h6>Etec Sales Gomes @ 2025</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
